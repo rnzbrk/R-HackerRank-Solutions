9f876bc5509d126c339fb19e9b8c1ebdcb071c54
Eren Ozberk <erenozberk@gmail.com>
2023-12-07 10:52:40 +0000

    Create Divisible_Sum_Pairs.r

---
+++ Divisible_Sum_Pairs.r
@@ -0,0 +1,14 @@
+divisibleSumPairs <- function(n, k, ar) {
+  result <- list()
+    for (i in 1:(n - 1)) {
+    for (j in (i + 1):n) {
+      pair_sum <- ar[i] + ar[j]
+      if (pair_sum %% k == 0) {
+        result <- append(result, list(c(ar[i], ar[j])))
+        n_tot <- length(result)
+      }
+    }
+  }
+  
+  return(n_tot)
+}
